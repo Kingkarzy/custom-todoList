a30cb322b83d7415a2dfc539381d497c483fd47c
Kingkarzy <43390918+Kingkarzy@users.noreply.github.com>
2023-01-13 13:35:08 +0100

    edit and delete button

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import './App.css'
 
 function App() {
   const [input, setInput] = useState("");
-  const [myList, setList] = useState([])
+  const [myList, setList] = useState([]);
 
   const submit = () => {
     // e.preventDefault();
-    if (input===""){
-      alert("Please enter a todo");
-    }else{
-      setList([...myList, input]);
-      setInput('');
-    }
+    // if (input===""){
+    //   alert("Please enter a todo");
+    // }else{
+    //   setList([...myList, input]);
+    //   setInput('');
+    // }
+
+    setList([...myList, input]);
+    setInput('');
+  }
+
+  const edit = (todo) => {
+    setInput(todo);
+  } 
+
+  const deleteTodo = (index) => {   
+   const newList = [...myList];
+   newList.splice(index, 1);
+   setList(newList);
   }
 
   return (
@@ -39,12 +52,27 @@ function App() {
 
       <div className='card'>
         <h1>My Todos</h1>
-        <ul>
-          <li className='card'>
+        <ul className='lists'>
+          <li className='card-list'>
           {
+            
             myList.map((todo, index) => (
-               <li key={index}>{todo}</li>
+              <>
+               <li 
+                key={index}
+                onClick={() => {edit(todo)}}
+                >
+                {todo}
+               </li>
+               <button
+                className='btn-danger'
+                onClick={() => deleteTodo(index)}
+                >
+                  delete
+                </button>
+             </>
              ))
+             
           }
           </li>
         </ul>
