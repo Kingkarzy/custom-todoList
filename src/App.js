97a6694ef9688518ff878772775b8bab9ac8ab12
Kingkarzy <43390918+Kingkarzy@users.noreply.github.com>
2023-02-02 22:59:43 +0100

    enter update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ import './App.css'
 function App() {
   const [input, setInput] = useState("");
   const [myList, setList] = useState([]);
+  const [isCompleted, setIsCompleted] = useState(true);
 
   const submit = () => {
-    // e.preventDefault();
-    // if (input===""){
-    //   alert("Please enter a todo");
-    // }else{
-    //   setList([...myList, input]);
-    //   setInput('');
-    // }
-
-    setList([...myList, input]);
-    setInput('');
+    if (input===""){
+      alert("Please enter a todo");
+    }else{
+      setList([...myList, input]);
+      setInput('');
+    }
   }
 
   const edit = (todo) => {
@@ -29,6 +26,17 @@ function App() {
    setList(newList);
   }
 
+  const handleChange = (todo) => {
+   if(isCompleted===false){
+    setIsCompleted(!isCompleted); 
+    console.log(`${todo} has been been marked incomplete`)
+   }else{
+    setIsCompleted(!isCompleted); 
+    console.log(`Yayy!!! ${todo} has been completed`)
+   }
+  }
+  
+
   return (
     <div className='container main'>
       <div className='container-sm'>
@@ -36,9 +44,15 @@ function App() {
         <div className='form'>
           <input 
             type='text'
+            className='enterTodo'
             placeholder='Enter todo'
             value={input}
             onChange={(e) => setInput(e.target.value)} 
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                submit();
+              }
+            }}
             />
           
           <button
@@ -52,29 +66,33 @@ function App() {
 
       <div className='card'>
         <h1>My Todos</h1>
-        <ul className='lists'>
-          <li className='card-list'>
+        <ul className='lists card-list'>
           {
             
-            myList.map((todo, index) => (
-              <>
-               <li 
-                key={index}
-                onClick={() => {edit(todo)}}
-                >
-                {todo}
-               </li>
-               <button
-                className='btn-danger'
-                onClick={() => deleteTodo(index)}
-                >
-                  delete
-                </button>
-             </>
-             ))
-             
+            myList.map((todo, index) => {
+              return(
+                <>
+                <li 
+                  key={index}
+                  onClick={() => {edit(todo)}}
+                  >
+                  {todo}
+                </li>
+                <input 
+                  type="checkbox" 
+                  value={todo}
+                  onChange={() => {handleChange(todo)}} 
+                />
+                <button
+                  className='btn-danger'
+                  onClick={() => deleteTodo(index)}
+                  >
+                    delete
+                  </button>
+              </>
+              );
+            })      
           }
-          </li>
         </ul>
       </div>
     </div>  
